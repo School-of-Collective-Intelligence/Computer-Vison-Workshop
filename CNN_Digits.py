6a9cde610d08fe809ce83c48c9d9bd1811a296bd
FatimaEzzahra Benmarrakchi <86061276+FzBenmarrakchi@users.noreply.github.com>
2022-10-17 23:22:52 +0100

    Update CNN_Digits.py

--- CNN_Digits.py
+++ CNN_Digits.py
@@ -35,9 +35,13 @@ test_img=keras.utils.normalize(test_img,axis=1)
 
 #Build the model
 model=Sequential()
-model.add(Conv2D(32,(3,3),input_shape=(28,28,1)))
+
+model.add(Conv2D(32,(3,3),activation='relu',input_shape=(28,28,1)))
 model.add(MaxPooling2D(3,3))
-model.add(Conv2D(16,(3,3)))
+
+model.add(Conv2D(16,(3,3),activation='relu'))
+model.add(MaxPooling2D(3,3))
+
 model.add(Flatten())
 model.add(Dense(300,activation='relu'))
 model.add(Dense(10,activation='softmax'))
